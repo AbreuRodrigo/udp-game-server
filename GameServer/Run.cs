2e42ee631c0c259a605e9b211c4c33861d228e5c
Rodrigo Abreu <rodrigo.abreu.ca@gmail.com>
2017-01-08 03:23:10 -0800

    Separating classes into their correct directories

---
+++ GameServer/Run.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Run
+{
+    static void Main(string[] args)
+    {
+        UDPServer server = new UDPServer(11000);
+        server.Start();
+    }
+}
